96dbc62757fe4bbd4c3a9b6375f228c0ed350a2e
Jaume Curto Castellví <jaumecurtojob@gmail.com>
2022-11-07 12:46:16 +0100

    Commit Dependencia POM y MainApp listo

--- clientes/src/main/java/controllers/ControladorVista.java
+++ clientes/src/main/java/controllers/ControladorVista.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import vista.*;
@@ -13,11 +14,19 @@ public class ControladorVista implements ActionListener {
 	private PanelOpciones panelOpciones;
 	private PanelFormularios panelFormularios;
 	
-	public void iniciarVista() {
-		panelOpciones = new PanelOpciones();
-		panelFormularios = new PanelFormularios();
-		cframe = new ClienteFrame(panelOpciones, panelFormularios);
+	public ControladorVista(ClienteFrame cframe, PanelOpciones panelOpciones, PanelFormularios panelFormularios) {
+		super();
+		this.cframe = cframe;
+		this.panelOpciones = panelOpciones;
+		this.panelFormularios = panelFormularios;
 		agregarEventos();
+	}
+
+	public void iniciarVista() {
+		cframe.setTitle("Clientes CRUD");
+		cframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		cframe.setBounds(100, 100, 345, 300);
+		cframe.setVisible(true);
 	}
 	
 	public void agregarEventos() {
